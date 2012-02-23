21de8f3d69dc6e74bf7367c1eba123402dea2135
Jerome Cornet <jerome@aldorande.net>
2012-02-23 21:21:48 +0100

    Moved free memory function to separate file to ease port on SDK simulator

---
+++ JIXos/JIXUtilities.ino
@@ -0,0 +1,36 @@
+//----------------------------------------------------------------------------------------
+// JIXos: JIX Clock operating system
+// Copyright (c) 2010-2011 Jérôme Cornet <jerome@aldorande.net>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------------------------
+// Utilities
+//----------------------------------------------------------------------------------------
+
+extern unsigned int __bss_end;
+extern void *__brkval;
+
+int get_free_memory()
+{
+  int free_memory;
+
+  if((int)__brkval == 0)
+    free_memory = ((int)&free_memory) - ((int)&__bss_end);
+  else
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+
+  return free_memory;
+}
+
--- JIXos/JIXos.ino
+++ JIXos/JIXos.ino
@@ -507,21 +507,6 @@ void kernel()
   }
 }
 
-extern unsigned int __bss_end;
-extern void *__brkval;
-
-int get_free_memory()
-{
-  int free_memory;
-
-  if((int)__brkval == 0)
-    free_memory = ((int)&free_memory) - ((int)&__bss_end);
-  else
-    free_memory = ((int)&free_memory) - ((int)__brkval);
-
-  return free_memory;
-}
-
 void loop()
 {
   Serial.print(F("JIXos version "));
