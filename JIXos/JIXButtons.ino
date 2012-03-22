46db8be2c7bb5c0b2346743d11deffd8fbf4eb8c
Jerome Cornet <jerome@aldorande.net>
2012-03-22 22:56:46 +0100

    Added missing files for button support

---
+++ JIXos/JIXButtons.ino
@@ -0,0 +1,59 @@
+//----------------------------------------------------------------------------------------
+// JIXos: JIX Clock operating system
+// Copyright (c) 2010-2011 Jérôme Cornet <jerome@aldorande.net>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------------------------
+// Buttons management
+//----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
+// Public API
+//----------------------------------------------------------------------------------------
+
+void buttonsInit();
+
+bool buttonsGetModeState();
+
+bool buttonsGetIncrementState();
+
+//----------------------------------------------------------------------------------------
+// Constants
+//----------------------------------------------------------------------------------------
+
+const unsigned int SETH_PIN = 4;
+const unsigned int SETM_PIN = 12;
+
+//----------------------------------------------------------------------------------------
+// Implementation
+//----------------------------------------------------------------------------------------
+
+void buttonsInit()
+{
+  pinMode(SETH_PIN, INPUT);
+  pinMode(SETM_PIN, INPUT);
+}
+
+bool buttonsGetModeState()
+{
+   return (digitalRead(SETH_PIN) == 0);
+}
+
+bool buttonsGetIncrementState()
+{
+  return (digitalRead(SETM_PIN) == 0);
+}
+
+
